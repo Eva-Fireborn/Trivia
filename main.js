eed0f04e40f23633953ceb0c5476819be230b186
Eva-Fireborn <Eva-Fireborn@users.noreply.github.com>
2019-01-07 18:02:30 +0100

    Fixed front page as div

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-
-
     let totalPoints = 0;
+    let isPaused = false;
 
     let temporary = localStorage.getItem('totalPoints');
     if (temporary !== null) {
@@ -13,8 +12,27 @@ $(document).ready(function() {
 
     let currentGame= [{"category":"Vehicles","type":"boolean","difficulty":"easy","question":"In 1993 Swedish car manufacturer Saab experimented with replacing the steering wheel with a joystick in a Saab 9000.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"The main character in the &quot;Half-Life&quot; franchise is named Morgan Freeman.","correct_answer":"False","incorrect_answers":["True"]},{"category":"History","type":"boolean","difficulty":"easy","question":"The Tiananmen Square protests of 1989 were held in Hong Kong.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Geography","type":"boolean","difficulty":"easy","question":"Alaska is the largest state in the United States.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"easy","question":"Tetris is the #1 best-selling video game of all-time.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Geography","type":"boolean","difficulty":"easy","question":"Toronto is the capital city of the North American country of Canada.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Animals","type":"boolean","difficulty":"easy","question":"Rabbits are rodents.","correct_answer":"False","incorrect_answers":["True"]},{"category":"Entertainment: Film","type":"boolean","difficulty":"easy","question":"In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.","correct_answer":"False","incorrect_answers":["True"]},{"category":"History","type":"boolean","difficulty":"easy","question":"In World War ll, Great Britian used inflatable tanks on the ports of Great Britain to divert Hitler away from Normandy\/D-day landing.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Sports","type":"boolean","difficulty":"easy","question":"In Rugby League, performing a &quot;40-20&quot; is punished by a free kick for the opposing team.","correct_answer":"False","incorrect_answers":["True"]}];
     let todayPoints = 0;
+    let username = localStorage.getItem('username');
+    if (username !== null) {
+        $('.content').css('display', 'none');
+        $('.background').css('display', 'none');
+        isPaused = true;
+        changeFunText();
+    } else {
+        $('.content').css('display', 'block');
+        $('.background').css('display', 'block');
+        isPaused = false;
+    }
 
+    $('#nextPage').click(event => {
+		let value = $('#nameInput').val();
+        localStorage.setItem('username', value);
+        username = value;
+        $('.content').css('display', 'none');
+        $('.background').css('display', 'none');
+        isPaused=true;
 
+  })
 
     $('#newGameButton').on('click', event => {
         $('.newGame').css('display', 'none');
@@ -73,7 +91,7 @@ $(document).ready(function() {
         changeBackground();
     });
 
-      let username = localStorage.getItem('username');
+      
 
 
 
@@ -125,6 +143,52 @@ $(document).ready(function() {
 
     }; //functon nextQuestion ends here.
 
+
+    //Tillhör animationen
+    const rhombus = '<svg viewBox="0 0 13 14"><path class="rhombus" d="M5.9,1.2L0.7,6.5C0.5,6.7,0.5,7,0.7,7.2l5.2,5.4c0.2,0.2,0.5,0.2,0.7,0l5.2-5.4 C12,7,12,6.7,11.8,6.5L6.6,1.2C6.4,0.9,6.1,0.9,5.9,1.2L5.9,1.2z M3.4,6.5L6,3.9c0.2-0.2,0.5-0.2,0.7,0l2.6,2.6 c0.2,0.2,0.2,0.5,0,0.7L6.6,9.9c-0.2,0.2-0.5,0.2-0.7,0L3.4,7.3C3.2,7.1,3.2,6.8,3.4,6.5L3.4,6.5z" /></svg>'
+  
+  const pentahedron = '<svg viewBox="0 0 561.8 559.4"><path class="pentahedron" d="M383.4,559.4h-204l-2.6-0.2c-51.3-4.4-94-37-108.8-83l-0.2-0.6L6,276.7l-0.2-0.5c-14.5-50,3.1-102.7,43.7-131.4 L212.1,23C252.4-7.9,310.7-7.9,351,23l163.5,122.5l0.4,0.3c39,30.3,56,82.6,42.2,130.3l-0.3,1.1l-61.5,198 C480.4,525.6,435.5,559.4,383.4,559.4z M185.5,439.4h195.2l61.1-196.8c0-0.5-0.3-1.6-0.7-2.1L281.5,120.9L120.9,241.2 c0,0.3,0.1,0.7,0.2,1.2l60.8,195.8C182.5,438.5,183.7,439.1,185.5,439.4z M441,240.3L441,240.3L441,240.3z"/></svg>'
+  const x = '<svg viewBox="0 0 12 12"> <path class="x" d="M10.3,4.3H7.7V1.7C7.7,0.8,7,0,6,0S4.3,0.8,4.3,1.7v2.5H1.7C0.8,4.3,0,5,0,6s0.8,1.7,1.7,1.7h2.5v2.5 C4.3,11.2,5,12,6,12s1.7-0.8,1.7-1.7V7.7h2.5C11.2,7.7,12,7,12,6S11.2,4.3,10.3,4.3z"/></svg>'
+  
+  const circle = '<svg x="0px" y="0px" viewBox="0 0 13 12"> <path class="circle" d="M6.5,0.1C3.4,0.1,0.8,2.8,0.8,6s2.6,5.9,5.7,5.9s5.7-2.7,5.7-5.9S9.7,0.1,6.5,0.1L6.5,0.1z M6.5,8.8 C5,8.8,3.8,7.6,3.8,6S5,3.2,6.5,3.2S9.2,4.4,9.2,6S8,8.8,6.5,8.8L6.5,8.8z"/> </svg>'
+  
+  const point = '<svg viewBox="0 0 12 12"> <path class="point" d="M6,7.5L6,7.5C5.1,7.5,4.5,6.9,4.5,6v0c0-0.9,0.7-1.5,1.5-1.5h0c0.9,0,1.5,0.7,1.5,1.5v0C7.5,6.9,6.9,7.5,6,7.5z "/> </svg>'
+  
+  function randomInt(min,max){
+      return Math.floor(Math.random()*(max-min+1)+min);
+  }
+  
+  const data = [point, rhombus, pentahedron, circle, x]
+  
+  
+  /*window.onblur = function() {
+      isPaused = true;
+  }.bind(this)
+  window.onfocus = function() {
+      isPaused = false;
+  }.bind(this)*/
+  
+  let particles = []
+  
+  setInterval(function(){
+    if (!isPaused){
+      particles.push(
+        new Particle(data[randomInt(0,data.length-1)], {
+         "x": (Math.random() * $(window).width()),
+         "y": $(window).height()
+        }, (1 + Math.random() * 3) )
+      )
+    }
+  }, 200)
+  
+  function update(){
+    particles = particles.filter(function(p){
+      return p.move()
+    })
+    requestAnimationFrame(update.bind(this))
+  }
+
+  update();
 }); //When ready function
 
 function changeBackground (number=7) {
@@ -153,3 +217,51 @@ function changeBackground (number=7) {
         break;
     }
 };
+class Particle{
+    
+    constructor(svg, coordinates, friction){
+      this.svg = svg
+      this.steps = ($(window).height())/2
+      this.item = null
+      this.friction = friction
+      this.coordinates = coordinates
+      this.position = this.coordinates.y
+      this.dimensions = this.render()
+      this.rotation = Math.random() > 0.5 ? "-" : "+"
+      this.scale = 0.5 + Math.random()
+      this.siner = 200 * Math.random()
+    }
+    
+    destroy(){
+      this.item.remove()
+    }
+    
+    move(){
+      this.position = this.position - this.friction
+      let top = this.position;
+      let left = this.coordinates.x + Math.sin(this.position*Math.PI/this.steps) * this.siner;
+      this.item.css({
+        transform: "translateX("+left+"px) translateY("+top+"px) scale(" + this.scale + ") rotate("+(this.rotation)+(this.position + this.dimensions.height)+"deg)"
+      })
+  
+      if(this.position < -(this.dimensions.height)){
+        this.destroy()
+        return false
+      }else{
+        return true
+      }
+    }
+    
+    render(){
+      this.item = $(this.svg, {
+        css: {
+          transform: "translateX("+this.coordinates.x+"px) translateY("+this.coordinates.y+"px)"
+        }
+      })
+      $("body").append(this.item)
+      return {
+        width: this.item.width(),
+        height: this.item.height()
+      }
+    }
+}
